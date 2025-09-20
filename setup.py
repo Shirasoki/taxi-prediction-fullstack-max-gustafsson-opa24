715eeb008a52cbcd9efa1ce0e057af9b57130467
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-09-20 22:30:26 +0200

    added plot

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="this package contains taxipred app",
     author="Max Gustafsson",
     author_email="author@mail.se",
-    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "ipykernel"],
+    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "ipykernel", "matplotlib", "seaborn"],
     package_dir={"": "src"},
     package_data={"taxipred": ["data/*.csv"]},
     packages=find_packages(),
