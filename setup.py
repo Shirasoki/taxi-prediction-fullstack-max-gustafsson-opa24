64bb4cba08ddef66b90358a8c58182d26a234784
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-10-02 18:11:19 +0200

    started on models

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="this package contains taxipred app",
     author="Max Gustafsson",
     author_email="author@mail.se",
-    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "ipykernel", "matplotlib", "seaborn"],
+    install_requires=["streamlit", "pandas", "fastapi", "uvicorn", "ipykernel", "matplotlib", "seaborn", "scikit-learn", "pathlib", "joblib"],
     package_dir={"": "src"},
     package_data={"taxipred": ["data/*.csv"]},
     packages=find_packages(),
--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,7 @@
 from importlib.resources import files
+from pathlib import Path
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
-
-# DATA_PATH = Path(__file__).parents[1] / "data"
+CLEANED_CSV_PATH = files("taxipred").joinpath("data/cleaned_data_v2.csv")
+#CLEANED_CSV_PATH = "src/taxipred/data/cleaned_data_v2.csv"
+#DATA_PATH = Path(__file__).parents[1] / "data"
