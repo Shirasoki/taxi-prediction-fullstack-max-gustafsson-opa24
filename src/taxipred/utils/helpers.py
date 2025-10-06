bae28af28b06478cdcfa5c74e098cfe03d4c763d
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-10-06 13:22:39 +0200

    working on fastapi

--- src/taxipred/utils/helpers.py
+++ src/taxipred/utils/helpers.py
@@ -1,11 +1,25 @@
 import requests 
 from urllib.parse import urljoin
 
-def read_api_endpoint(endpoint = "/", base_url = "http://127.0.0.1:8000"):
+def read_api_endpoint(endpoint = "/read", base_url = "http://127.0.0.1:8000"):
     url = urljoin(base_url, endpoint)
     response = requests.get(url)
     
     return response
+
+
+def post_api_endpoint(endpoint = "/post", base_url = "http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    response = requests.post(url)
+    
+    return response
+
+def predict_api_endpoint(data, endpoint = "/predict", base_url = "http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    response = requests.post(url, json=data)
+    
+    return response
+
 
 # TODO:
 # post_api_endpoint
