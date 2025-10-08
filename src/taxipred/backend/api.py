068db0031d0353dbaadea1400d1c9aa31647c367
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-10-08 22:11:00 +0200

    fixed api and posts

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
+from pydantic import BaseModel, conint, confloat
+from datetime import datetime
+from pathlib import Path
+import joblib
+import pandas as pd
+import requests
+from urllib.parse import urljoin
+from taxipred.backend.data_processing import TaxiData, PredictionResponse, TaxiInput, TaxiFareOutput
+from taxipred.utils.constants import TAXI_MODEL_PATH, CLEANED_CSV_PATH
 
 app = FastAPI()
 
 taxi_data = TaxiData()
+BASE_URL = ("http://127.0.0.1:8000")
 
 @app.get("/taxi/")
 async def read_taxi_data():
     return taxi_data.to_json()
 
+@app.post("/taxi/predict", response_model=PredictionResponse)
+async def predict_taxi_price(payload: TaxiInput):
+    data_to_predict = pd.DataFrame([payload.model_dump()])
+    model = joblib.load(TAXI_MODEL_PATH)
+    prediction = model.predict(data_to_predict)
+    
+    return {"Predicted_Price": prediction[0]}
+
+@app.post("/taxi/fares/predict", response_model=TaxiFareOutput)
+async def predict_taxi_fares(payload: TaxiInput):
+    data_to_predict = pd.DataFrame([payload.model_dump()])
+    model = joblib.load(TAXI_MODEL_PATH)
+    prediction = model.predict(data_to_predict)
+    
+    return {
+        "Base_Fare": prediction[0][0],
+        "Per_Km_Rate": prediction[0][1],
+        "Per_Minute_Rate": prediction[0][2]
+    }
 
 #test test test
--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -1,14 +1,30 @@
-from taxipred.utils.constants import TAXI_CSV_PATH
+from taxipred.utils.constants import TAXI_CSV_PATH, CLEANED_CSV_PATH
 import pandas as pd
 import json
+from pydantic import BaseModel, Field
+from typing import Annotated
+
 
 
 class TaxiData:
     def __init__(self):
-        self.df = pd.read_csv(TAXI_CSV_PATH)
+        self.df = pd.read_csv(CLEANED_CSV_PATH)
 
     def to_json(self):
         return json.loads(self.df.to_json(orient = "records"))
+    
+class TaxiInput(BaseModel):
+    Trip_Distance_km: float = Field(ge=1.2, le=50)
+    Base_Fare: float = Field(ge=2, le=5)
+    Per_Km_Rate: float = Field(ge=0.5, le=2)
+    Per_Minute_Rate: float = Field(ge=0.1, le=0.5)
+    Trip_Duration_Minutes: float = Field(ge=5, le=120)
 
+class PredictionResponse(BaseModel):
+    Predicted_Price: float
 
-#Test
+class TaxiFareOutput(BaseModel):
+    Base_Fare: float = Field(ge=2, le=5)
+    Per_Km_Rate: float = Field(ge=0.5, le=2)
+    Per_Minute_Rate: float = Field(ge=0.1, le=0.5)
+
