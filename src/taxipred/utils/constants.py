e66009d520eea46278a7a778ba2c5838076942e7
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-10-08 22:13:25 +0200

    last bit to make it all work

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -3,5 +3,8 @@ from pathlib import Path
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 CLEANED_CSV_PATH = files("taxipred").joinpath("data/cleaned_data_v2.csv")
+
+TAXI_MODEL_PATH  = files("taxipred").joinpath("models/random_forest_model.joblib")
+
 #CLEANED_CSV_PATH = "src/taxipred/data/cleaned_data_v2.csv"
 #DATA_PATH = Path(__file__).parents[1] / "data"
--- src/taxipred/utils/helpers.py
+++ src/taxipred/utils/helpers.py
@@ -1,20 +1,22 @@
 import requests 
 from urllib.parse import urljoin
 
-def read_api_endpoint(endpoint = "/read", base_url = "http://127.0.0.1:8000"):
+BASE_URL = "http://127.0.0.1:8000"
+
+def read_api_endpoint(endpoint = "/taxi/", base_url = BASE_URL):
     url = urljoin(base_url, endpoint)
     response = requests.get(url)
     
     return response
 
 
-def post_api_endpoint(endpoint = "/post", base_url = "http://127.0.0.1:8000"):
+def post_api_endpoint(endpoint = "/taxi/post", base_url = BASE_URL):
     url = urljoin(base_url, endpoint)
     response = requests.post(url)
     
     return response
 
-def predict_api_endpoint(data, endpoint = "/predict", base_url = "http://127.0.0.1:8000"):
+def predict_api_endpoint(data, endpoint = "/taxi/predict", base_url = BASE_URL):
     url = urljoin(base_url, endpoint)
     response = requests.post(url, json=data)
     
@@ -23,3 +25,4 @@ def predict_api_endpoint(data, endpoint = "/predict", base_url = "http://127.0.0
 
 # TODO:
 # post_api_endpoint
+# testet
